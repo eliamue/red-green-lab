01c6cf3712d843c27ab7787d7d572d190a6631f7
Elise Muellerleile <eliamue@live.com>
2021-07-19 19:53:56 -0500

    name test written

---
+++ getname.js
@@ -0,0 +1,5 @@
+const getNames = () => {
+  return ['Teen Wolf', 'Melhissandre', 'Zoey', 'Sir Digby Chicken Caesar'];
+};
+
+export default getNames;
---
+++ getname.test.js
@@ -0,0 +1,12 @@
+import getNames from './getname';
+
+describe('getNames', () => {
+  it('gets a list of names', async () => {
+    const stats = { name: 'Teen Wolf', age: 999, sex: 'female', species: 'cat' };
+    const expected = 'Teen Wolf';
+    const actual = getNames(stats);
+
+    expect(actual).toContainEqual(expected);
+  });
+
+});
